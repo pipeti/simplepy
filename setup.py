4b8975c5fbe888080a6a8666deb3a1b01eaa9bd0
pipeti <pipeti@users.noreply.github.com>
2018-11-30 20:37:03 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simplepy',
+      version='0.1',
+      description='Simple-pie!!!',
+      url='http://github.com/pipeti/simplepy',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['simplepy'],
+      zip_safe=False)
