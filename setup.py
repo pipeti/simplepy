8eaa58942cf7c0c91fcca20b422f310a9280af83
pipeti <pipeti@users.noreply.github.com>
2018-11-30 20:46:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,12 @@ setup(name='simplepy',
       author_email='flyingcircus@example.com',
       license='MIT',
       packages=['simplepy'],
+      install_requires=[
+            'psycopg2>=2.7.3.2',
+            'pytz>=2018.4',
+            'gunicorn>=19.7.1',
+            'jsonschema>=2.5',
+            'Flask>=0.12.1',
+            'kafka-python>=1.3.3'
+      ]
       zip_safe=False)
