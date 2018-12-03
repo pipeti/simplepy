f7a377b71df3c414824e3fd8bb9fbbfdc8bb2a9a
pipeti <pipeti@users.noreply.github.com>
2018-12-03 09:14:36 +0100

    Update __init__.py

--- simplepy/__init__.py
+++ simplepy/__init__.py
@@ -4,6 +4,15 @@ import gunicorn
 import jsonschema
 import Flask
 import kafka-python
+import click
+import Werkzeug
+import itsdangerous
+import functools32
+import jsonschema
+import markupsafe
+import jinja2
+import meld3
+import supervisorimport
 
 def joke():
     return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
